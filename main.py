2a9726fbaa841a6ab72b95aa38b6f5f520f69c7b
akashgit77 <105647543+akashgit77@users.noreply.github.com>
2025-02-17 15:22:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1480,40 +1480,54 @@ class TestPages:
     def test_upload_file_less_than_5mb(self):
         self.driver.implicitly_wait(30)
         self.login_to_canonizer_app()
-        time.sleep(10)
         self.driver.get(UPLOAD_FILE_URL)
         image = "/home/vivekkumar/PycharmProjects/Canonizer_UX _Github/UI/image.png"
         upload_image = self.driver.find_element(By.CSS_SELECTOR, "input[type='file']")
         upload_image.send_keys(image)
-        time.sleep(5)
         result = self.driver.find_element(*ProfileInfoIdentifiersPage.UPLOAD_BUTTON).text
         assert "Upload" in result
 
     def test_upload_file_more_than_5mb(self):
         self.driver.implicitly_wait(30)
         self.login_to_canonizer_app()
-        time.sleep(10)
         self.driver.get(UPLOAD_FILE_URL)
         image = "/home/vivekkumar/PycharmProjects/Canonizer_UX _Github/UI/10mb.jpg"
         upload_image = self.driver.find_element(By.CSS_SELECTOR, "input[type='file']")
         upload_image.send_keys(image)
-        time.sleep(5)
         result = self.driver.find_element(*ProfileInfoIdentifiersPage.UPLOAD_SIZE_EXCEEDED).text
         assert "This file is exceeding the max limit and will not be uploaded" in result
 
     def test_uploading_file_less_than_5mb(self):
         self.driver.implicitly_wait(30)
         self.login_to_canonizer_app()
-        time.sleep(10)
         self.driver.get(UPLOAD_FILE_URL)
         image = "/home/vivekkumar/PycharmProjects/Canonizer_UX _Github/UI/image.png"
         upload_image = self.driver.find_element(By.CSS_SELECTOR, "input[type='file']")
         upload_image.send_keys(image)
-        time.sleep(5)
         self.driver.find_element(*ProfileInfoIdentifiersPage.UPLOAD_FILE_NAME).send_keys("test file upload")
         self.driver.find_element(*ProfileInfoIdentifiersPage.UPLOAD_BUTTON).click()
         result = self.driver.find_element(*ProfileInfoIdentifiersPage.FILE_UPOADED_NUMBER).text
         assert "5 Files, 0 Folder" in result
+
+    def test_profile_page_name_change(self):
+        self.driver.implicitly_wait(30)
+        self.login_to_canonizer_app()
+        self.driver.get(PROFILE_PAGE)
+        self.driver.find_element(*ProfileInfoIdentifiersPage.FIRST_NAME).send_keys("ing")
+        self.driver.find_element(*ProfileInfoIdentifiersPage.SAVE_PROFILE_CHANGES).click()
+        result = self.driver.find_element(*ProfileInfoIdentifiersPage.USERNAME_UPPER).text
+
+        assert "Akashing" in result
+
+    def test_cafe_text_in_address_bar(self):
+        self.driver.implicitly_wait(30)
+        self.login_to_canonizer_app()
+        self.driver.get(PROFILE_PAGE)
+        self.driver.find_element(*ProfileInfoIdentifiersPage.ADDRESS_LINE).send_keys("Café,")
+        self.driver.find_element(*ProfileInfoIdentifiersPage.SAVE_PROFILE_CHANGES).click()
+        result = self.driver.find_element(*ProfileInfoIdentifiersPage.PROFILE_UPDATED_POP_UP).text
+
+        assert "Profile updated successfully." in result
 
     
     def teardown_method(self):
