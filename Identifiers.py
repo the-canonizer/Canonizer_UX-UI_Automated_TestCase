2187432f47b247d12908b8fbd86fdd83a1e774d1
akashgit77 <105647543+akashgit77@users.noreply.github.com>
2025-02-19 17:30:02 +0530

    Update Identifiers.py

--- Identifiers.py
+++ Identifiers.py
@@ -686,6 +686,7 @@ class BrowsePageIdentifiers(object):
     NOTIFICATION_BELL = (By.XPATH, "/html/body/div[1]/section/header/div/nav/ul/li[11]/span/span")
     TOPIC_TAG = (By.ID, "browse-topic-tags-item")
     TOPIC_SEARCH_COUNT = (By.XPATH, "/html/body/div[1]/section/section/main/div[2]/aside/div/div/div/a[2]/button/span[2]")
+    VIDEOS_THUMBNAIL = (By.ID, "videos-category-thumbnail")
     
 class UploadFileIdentifiers(object):
     """
