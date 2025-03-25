83386a98154b6e5a0b9cdc8d713bf11b6b4b346e
akashgit77 <105647543+akashgit77@users.noreply.github.com>
2025-03-25 12:28:45 +0530

    Update CanonizerUploadFile.py

--- CanonizerUploadFile.py
+++ CanonizerUploadFile.py
@@ -36,29 +36,16 @@ class CanonizerUploadFile(Page):
         self.driver.get(UPLOAD_FILE_URL)
         return CanonizerUploadFile(self.driver)
 
-    def upload_file_with_non_admin(self):
-        self.driver.implicitly_wait(30)
-        CanonizerLoginPage(self.driver).click_on_login_page_button().verify_the_login_with_user2(DEFAULT_USER_2, DEFAULT_PASS_2)
-        self.driver.get(UPLOAD_FILE_URL)
-        return CanonizerUploadFile(self.driver)
 
     def upload_file_with_admin(self):
         self.driver.implicitly_wait(30)
         self.driver.get(UPLOAD_FILE_URL)
         return CanonizerUploadFile(self.driver)
 
-    def upload_file_less_than_5mb(self):
-        self.driver.implicitly_wait(30)
-        self.driver.get(UPLOAD_FILE_URL)
-        image = "/home/akash/PycharmProjects/Canonizer_UX _Github/UI/image.png"
-        upload_image = self.driver.find_element(By.CSS_SELECTOR, "input[type='file']")
-        upload_image.send_keys(image)
-        return CanonizerUploadFile(self.driver)
-
     def upload_file_more_than_5mb(self):
         self.driver.implicitly_wait(30)
         self.driver.get(UPLOAD_FILE_URL)
-        image = "/home/akash/PycharmProjects/Canonizer_UX _Github/UI/10mb.jpg"
+        image = "/home/vivekkumar/PycharmProjects/Canonizer_UX _Github/UI/10mb.jpg"
         upload_image = self.driver.find_element(By.CSS_SELECTOR, "input[type='file']")
         upload_image.send_keys(image)
         return CanonizerUploadFile(self.driver)
@@ -66,9 +53,34 @@ class CanonizerUploadFile(Page):
     def uploading_file_less_than_5mb(self):
         self.driver.implicitly_wait(30)
         self.driver.get(UPLOAD_FILE_URL)
-        image = "/home/akash/PycharmProjects/Canonizer_UX _Github/UI/image.png"
+        image = "/home/vivekkumar/PycharmProjects/Canonizer_UX _Github/UI/image.png"
         upload_image = self.driver.find_element(By.CSS_SELECTOR, "input[type='file']")
         upload_image.send_keys(image)
-        self.driver.find_element(*ProfileInfoIdentifiersPage.UPLOAD_FILE_NAME).send_keys("test file upload")
-        self.driver.find_element(*ProfileInfoIdentifiersPage.UPLOAD_BUTTON).click()
+        print("wrinting file name")
+        add_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+        self.driver.find_element(*HomePageIdentifiers.UPLOAD_FILE_NAME).send_keys("test"+add_name)
+        self.driver.find_element(*HomePageIdentifiers.UPLOAD_BUTTON).click()
+        return CanonizerUploadFile(self.driver)
+
+
+    def upload_in_create_new_folder(self):
+        self.driver.implicitly_wait(30)
+        self.driver.get(UPLOAD_FILE_URL)
+        self.driver.find_element(By.ID, "createFolderBtn").click()
+        add_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+        self.driver.find_element(By.ID, "Create a Folder_Folder Name").send_keys("new_folder"+add_name)
+        action = ActionChains(self.driver)
+        action.key_down(Keys.ENTER).perform()
+        return CanonizerUploadFile(self.driver)
+
+
+    def upload_file_in_new_folder(self):
+        self.driver.implicitly_wait(30)
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/section/section/main/div/div/div/form/div/div[2]/div/div[2]/div[2]/div[7]/div/div/div/div/div[1]/span").click()
+        add_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+        image = "/home/vivekkumar/PycharmProjects/Canonizer_UX _Github/UI/image.png"
+        upload_image = self.driver.find_element(By.CSS_SELECTOR, "input[type='file']")
+        upload_image.send_keys(image)
+        self.driver.find_element(*HomePageIdentifiers.UPLOAD_FILE_NAME).send_keys("test"+add_name)
+        self.driver.find_element(*HomePageIdentifiers.UPLOAD_BUTTON).click()
         return CanonizerUploadFile(self.driver)
