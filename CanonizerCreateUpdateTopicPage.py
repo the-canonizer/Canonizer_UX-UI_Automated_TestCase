ab8065b8547ac16da499060bc4b7fa81e5911ab7
akashgit77 <105647543+akashgit77@users.noreply.github.com>
2025-04-01 15:24:16 +0530

    Update CanonizerCreateUpdateTopicPage.py

--- CanonizerCreateUpdateTopicPage.py
+++ CanonizerCreateUpdateTopicPage.py
@@ -24,12 +24,14 @@ class CanonizerCreateNewTopic(Page):
 
     def click_create_topic_button_without_login(self):
         self.driver.implicitly_wait(30)
-        self.driver.find_element(By.XPATH, "/html/body/div[1]/section/header/div/nav/ul/li[1]/a/span").click()
+        self.driver.find_element(By.ID, "create-topic-link").click()
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, "login-submit-btn")))
         return CanonizerCreateNewTopic(self.driver)
 
     def click_create_topic_button(self):
         self.driver.implicitly_wait(30)
-        self.driver.find_element(By.XPATH, "/html/body/div[1]/section/header/div/nav/ul/li[1]/a/span[1]").click()
+        self.driver.find_element(By.ID, "create-topic-link").click()
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "cancel-btn")))
         return CanonizerCreateNewTopic(self.driver)
 
     def create_topic_button(self):
@@ -38,7 +40,7 @@ class CanonizerCreateNewTopic(Page):
 
     def enter_topic_name(self, topic_name):
         self.driver.implicitly_wait(30)
-         WebDriverWait(self.driver, 2).until(EC.presence_of_element_located((By.ID, 'create_new_topic_topic_name')))
+        WebDriverWait(self.driver, 2).until(EC.presence_of_element_located((By.ID, 'create_new_topic_topic_name')))
         self.find_element(*CreateTopicIdentifiers.TOPIC_NAME).send_keys(topic_name)
 
     def entering_data_fields(self, topic_name):
@@ -126,6 +128,8 @@ class CanonizerCreateNewTopic(Page):
     def create_topic_with_blank_topic(self):
         self.driver.implicitly_wait(30)
         self.create_topic("     ")
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, "create_new_topic_topic_name_help")))
+
         return CanonizerCreateNewTopic(self.driver)
 
     def create_topic_with_same_topic(self, topic_name):
@@ -198,209 +202,100 @@ class CanonizerUpdateTopicPage(Page):
     def load_topic_history_page(self):
 
         self.driver.implicitly_wait(30)
-        self.driver.find_element(By.XPATH, "/html/body/div[1]/section/section/main/div/div/div/div[1]/div[2]/div[2]/a/span/img").click()
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/section/section/main/div/div/div/div[1]/div[1]/div[2]/div[2]/a/span").click()
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "threedot_dropdown_manage_topic_btn__menu_item_text")))
         self.driver.find_element(By.ID, "threedot_dropdown_manage_topic_btn__menu_item_text").click()
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "topic-history-container")))
         self.driver.find_element(By.XPATH, "/html/body/div[1]/section/section/main/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/button/span").click()
-    def verify_topic_name_on_topic_history_page(self, topic_name):
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.CLASS_NAME, 'siteHeader_navWrap__yilWi false')))
-        except TimeoutException:
-            pass
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "history-page-back-button")))
 
-             Browse to Browse Page
-        self.hover(*CampForumIdentifiers.BROWSE)
-        self.find_element(*CampForumIdentifiers.BROWSE).click()
+        return CanonizerUpdateTopicPage(self.driver)
 
-        self.hover(*CampForumIdentifiers.SEARCH_TOPIC)
-        self.find_element(*CampForumIdentifiers.SEARCH_TOPIC).send_keys(topic_name)
-        self.hover(*CampForumIdentifiers.SEARCH_ICON)
-        self.find_element(*CampForumIdentifiers.SEARCH_ICON).click()
-        self.hover(*CampForumIdentifiers.TOPIC_CLICK)
-        self.find_element(*CampForumIdentifiers.TOPIC_CLICK).click()
-        try:
-            WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.CLASS_NAME, 'ant-btn ant-btn-default btn-green')))
-        except TimeoutException:
-            pass
-        self.hover(*UpdateTopicIdentifiers.TOPIC_NAME_ON_CURRENT_TOPIC_RECORD)
-        text1 = self.find_element(*UpdateTopicIdentifiers.TOPIC_NAME_ON_CURRENT_TOPIC_RECORD).text
-        self.hover(*UpdateTopicIdentifiers.MANAGE_EDIT_TOPIC_BUTTON)
-        self.find_element(*UpdateTopicIdentifiers.MANAGE_EDIT_TOPIC_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.TOPIC_NAME_ON_HISTORY_PAGE)
-        text2 = self.find_element(*UpdateTopicIdentifiers.TOPIC_NAME_ON_HISTORY_PAGE).text
-        if text1 == text2:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Text not matching")
 
-    def verify_submitter_nick_name_link_on_user_profile(self):
-        self.hover(*UpdateTopicIdentifiers.SUBMITTER_NICK_NAME_LINK)
-        text1 = self.find_element(*UpdateTopicIdentifiers.SUBMITTER_NICK_NAME_LINK).text
-        self.find_element(*UpdateTopicIdentifiers.SUBMITTER_NICK_NAME_LINK).click()
-        self.hover(*UpdateTopicIdentifiers.NICK_NAME_ON_PROFILE)
-        text2 = self.find_element(*UpdateTopicIdentifiers.NICK_NAME_ON_PROFILE).text
-        if text1 == text2:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Text not matching")
+
 
     def verify_submit_topic_update_button(self):
-        self.hover(*UpdateTopicIdentifiers.SUBMIT_UPDATE)
-        self.find_element(*UpdateTopicIdentifiers.SUBMIT_UPDATE).click()
-        self.hover(*UpdateTopicIdentifiers.TOPIC_UPDATE_TITLE)
-        page_title = self.find_element(*UpdateTopicIdentifiers.TOPIC_UPDATE_TITLE).text
-        if page_title == message['Update_Topic']['TOPIC_UPDATE_TITLE']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Title not found or is not matching")
+        self.driver.implicitly_wait(30)
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/section/section/main/div/div/div[2]/div/div/div[2]/div/div[2]/div[1]/button/span").click()
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "create-topic-btn")))
 
-    def update_topic_with_duplicate_name(self, duplicate_topic_name):
-        self.verify_submit_topic_update_button()
+        return CanonizerUpdateTopicPage(self.driver)
 
-        for i in range(0, 30):
-            self.find_element(*UpdateTopicIdentifiers.UPDATE_TOPIC_NAME).send_keys(Keys.BACKSPACE)
-        self.hover(*UpdateTopicIdentifiers.UPDATE_TOPIC_NAME)
-        self.find_element(*UpdateTopicIdentifiers.UPDATE_TOPIC_NAME).send_keys(duplicate_topic_name)
-        self.find_element(By.ID, "edit_summary").send_keys("new summary")
-        self.hover(*UpdateTopicIdentifiers.SUBMIT_UPDATE_BUTTON)
-        self.find_element(*UpdateTopicIdentifiers.SUBMIT_UPDATE_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.DUPLICATE_TOPIC_NAME_ERROR)
-        error = self.find_element(*UpdateTopicIdentifiers.DUPLICATE_TOPIC_NAME_ERROR).text
-        if error == message['Update_Topic']['DUPLICATE_TOPIC_NAME_ERROR']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Error not found or is not matching")
+
+    def update_topic_name(self):
+        self.driver.implicitly_wait(30)
+
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "create-topic-btn")))
+        self.driver.find_element(*UpdateTopicIdentifiers.UPDATE_TOPIC_NAME).send_keys("Test")
+        self.driver.find_element(*UpdateTopicIdentifiers.SUBMIT_UPDATE_BUTTON).click()
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/section/section/main/div/div/div[2]/div/div[1]/div[2]/div/div[3]/div[1]/button[1]/span")))
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/section/section/main/div/div/div[2]/div/div[1]/div[2]/div/div[3]/div[1]/button[1]/span").click()
+
+        return CanonizerUpdateTopicPage(self.driver)
 
     def update_topic_name_and_verify_submit_update_button(self, topic_name):
         self.verify_submit_topic_update_button()
-
-        for i in range(0, 30):
-            self.find_element(*UpdateTopicIdentifiers.UPDATE_TOPIC_NAME).send_keys(Keys.BACKSPACE)
-        self.hover(*UpdateTopicIdentifiers.UPDATE_TOPIC_NAME)
         self.find_element(*UpdateTopicIdentifiers.UPDATE_TOPIC_NAME).send_keys(topic_name)
-
         self.find_element(By.ID, "edit_summary").send_keys("new summary")
-        self.hover(*UpdateTopicIdentifiers.SUBMIT_UPDATE_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.SUBMIT_UPDATE_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.TOPIC_HISTORY_TITLE)
-        title = self.find_element(*UpdateTopicIdentifiers.TOPIC_HISTORY_TITLE).text
-        if title == message['Update_Topic']['TOPIC_HISTORY_TITLE']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Error not found or is not matching")
+
+        return CanonizerUpdateTopicPage(self.driver)
+
 
     def verify_cancel_button_functionality_on_topic_update_page(self):
         self.verify_submit_topic_update_button()
-        self.hover(*UpdateTopicIdentifiers.CANCEL_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.CANCEL_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.TOPIC_HISTORY_TITLE)
-        page_title = self.find_element(*UpdateTopicIdentifiers.TOPIC_HISTORY_TITLE).text
-        if page_title == message['Update_Topic']['TOPIC_HISTORY_TITLE']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Title not found or is not matching")
+        return CanonizerUpdateTopicPage(self.driver)
+
 
     def verify_preview_button_functionality_on_topic_update_page(self):
         self.verify_submit_topic_update_button()
-        self.hover(*UpdateTopicIdentifiers.PREVIEW_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.PREVIEW_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.TOPIC_PREVIEW_TITLE)
-        page_title = self.find_element(*UpdateTopicIdentifiers.TOPIC_PREVIEW_TITLE).text
-        if page_title == message['Update_Topic']['TOPIC_PREVIEW_TITLE']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Title not found or is not matching")
+        return CanonizerUpdateTopicPage(self.driver)
 
     def verify_submitter_nick_name_on_preview_modal(self):
         self.verify_submit_topic_update_button()
-        self.hover(*UpdateTopicIdentifiers.PREVIEW_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.PREVIEW_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.SUBMITTER_NICK_NAME_LINK_ON_PREVIEW_MODAL)
-        text1 = self.find_element(*UpdateTopicIdentifiers.SUBMITTER_NICK_NAME_LINK_ON_PREVIEW_MODAL).text
         self.find_element(*UpdateTopicIdentifiers.SUBMITTER_NICK_NAME_LINK_ON_PREVIEW_MODAL).click()
-        self.hover(*UpdateTopicIdentifiers.NICK_NAME_ON_PROFILE)
-        text2 = self.find_element(*UpdateTopicIdentifiers.NICK_NAME_ON_PROFILE).text
-        if text1 == text2:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Text not matching")
+        return CanonizerUpdateTopicPage(self.driver)
+
 
     def verify_cancel_button_on_preview_modal(self):
         self.verify_submit_topic_update_button()
-        self.hover(*UpdateTopicIdentifiers.PREVIEW_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.PREVIEW_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.CANCEL_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.CANCEL_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.TOPIC_UPDATE_TITLE)
-        error = self.find_element(*UpdateTopicIdentifiers.TOPIC_UPDATE_TITLE).text
-        if error == message['Update_Topic']['TOPIC_UPDATE_TITLE']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Error not found or is not matching")
+        return CanonizerUpdateTopicPage(self.driver)
+
 
     def verify_compare_topics_button_functionality(self):
-        self.hover(*UpdateTopicIdentifiers.COMPARE_CHECKBOX1)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_CHECKBOX1).click()
-        self.hover(*UpdateTopicIdentifiers.COMPARE_CHECKBOX2)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_CHECKBOX2).click()
-
-        self.hover(*UpdateTopicIdentifiers.COMPARE_TOPIC_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_TOPIC_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.TOPIC_HISTORY_COMPARISON_TITLE)
-        page_title = self.find_element(*UpdateTopicIdentifiers.TOPIC_HISTORY_COMPARISON_TITLE).text
-        if page_title == message['Update_Topic']['TOPIC_HISTORY_COMPARISON_TITLE']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Title not found or is not matching")
+        return CanonizerUpdateTopicPage(self.driver)
 
     def verify_agreement_link_on_topic_comparison_page(self):
-        self.hover(*UpdateTopicIdentifiers.COMPARE_CHECKBOX1)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_CHECKBOX1).click()
-        self.hover(*UpdateTopicIdentifiers.COMPARE_CHECKBOX2)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_CHECKBOX2).click()
-        self.hover(*UpdateTopicIdentifiers.COMPARE_TOPIC_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_TOPIC_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.AGREEMENT_LINK)
         self.find_element(*UpdateTopicIdentifiers.AGREEMENT_LINK).click()
-        self.hover(*CreateTopicIdentifiers.TOPIC_PAGE)
-        topic_page_confirmation = self.find_element(*CreateTopicIdentifiers.TOPIC_PAGE).text
-        if topic_page_confirmation == message['Create_Topic']['CREATE_TOPIC_TITLE']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Page not found")
+
+        return CanonizerUpdateTopicPage(self.driver)
+
 
     def verify_create_topic_button_functionality_on_topic_comparison_page(self):
-        self.hover(*UpdateTopicIdentifiers.COMPARE_CHECKBOX1)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_CHECKBOX1).click()
-        self.hover(*UpdateTopicIdentifiers.COMPARE_CHECKBOX2)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_CHECKBOX2).click()
-        self.hover(*UpdateTopicIdentifiers.COMPARE_TOPIC_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_TOPIC_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.CREATE_TOPIC_)
         self.find_element(*UpdateTopicIdentifiers.CREATE_TOPIC_).click()
-        self.hover(*CreateTopicIdentifiers.TOPIC_PAGE_TITLE)
-        page_title = self.find_element(*CreateTopicIdentifiers.TOPIC_PAGE_TITLE).text
-        if page_title == message['Create_Topic']['NEW_TOPIC_PAGE_TITLE']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Title not found or  is not matching")
+
+        return CanonizerUpdateTopicPage(self.driver)
 
     def verify_create_camp_button_functionality_on_topic_comparison_page(self):
-        self.hover(*UpdateTopicIdentifiers.COMPARE_CHECKBOX1)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_CHECKBOX1).click()
-        self.hover(*UpdateTopicIdentifiers.COMPARE_CHECKBOX2)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_CHECKBOX2).click()
-        self.hover(*UpdateTopicIdentifiers.COMPARE_TOPIC_BUTTON)
         self.find_element(*UpdateTopicIdentifiers.COMPARE_TOPIC_BUTTON).click()
-        self.hover(*UpdateTopicIdentifiers.CREATE_CAMP)
         self.find_element(*UpdateTopicIdentifiers.CREATE_CAMP).click()
-        self.hover(*CreateCampIdentifiers.NEW_CAMP_TITLE)
-        page_title = self.find_element(*CreateCampIdentifiers.NEW_CAMP_TITLE).text
-        if page_title == message['Create_Camp']['CREATE_CAMP_TITLE']:
-            return CanonizerUpdateTopicPage(self.driver)
-        else:
-            print("Title not found or is not matching")
+        return CanonizerUpdateTopicPage(self.driver)
 
     def verify_back_arrow_icon_on_topic_comparison_page(self):
         self.hover(*UpdateTopicIdentifiers.COMPARE_CHECKBOX1)
